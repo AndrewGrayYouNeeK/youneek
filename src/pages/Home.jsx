709d7f5d4bca8de5e9e4df6c02054868acee9ba5
Andrew Gray <youneekartifacts@gmail.com>
2026-03-13 05:23:22 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,7 @@ export default function Home() {
       <TransparencySection />
       <ImpactSection />
       <RoadmapSection />
+      <SwapWidget />
       <StakingSection />
       <DonationsSection />
       <ApplySection />
